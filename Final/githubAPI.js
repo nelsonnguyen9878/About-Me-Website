f44b050ac9f3cd5759ce5e376fee5fa62678fd7b
Nelson Nguyen <nelsonnguyen9878@gmail.com>
2026-06-09 16:29:18 -0700

    Initial commit

---
+++ Final/githubAPI.js
@@ -0,0 +1,35 @@
+const username = "nelsonnguyen9878";
+
+
+fetch(`https://api.github.com/users/${username}/repos`)
+  .then((response) => response.json())
+  .then((repos) => {
+
+    const container =
+      document.getElementById("repo-container");
+
+    repos.forEach((repo) => {
+
+      const card =
+      document.createElement("div");
+
+      card.classList.add("repo-card");
+
+      card.innerHTML = `
+        <div>
+          <h3>${repo.name}</h3>
+          <p>${repo.description || "No description"}</p>
+        </div>
+
+        <div>
+          ⭐ ${repo.stargazers_count}
+        </div>
+
+        <div>
+          ${repo.language || "N/A"}
+        </div>
+      `;
+
+      container.appendChild(card);
+    });
+  });
